e7530a65d32c8972b785781fec98839172ca41b7
Ankesiqi <1261807915@qq.com>
2025-03-26 22:55:06 +0800

    加入16bit 寄存器地址的函数
    加入I2C错误处理函数
    统一变量的类型（u8 ->uin8_t)
    改变读写多个字节的函数名称
    修复读写多个字节函数里的错误

--- 005串口打印信息/STM32F407_ProjectTemplate/bsp/Response/response.c
+++ 005串口打印信息/STM32F407_ProjectTemplate/bsp/Response/response.c
@@ -10,7 +10,6 @@ int do_process(usart_data_typed *udata)
     if (Header != 0x55)
 
     {
-        // printf("\n 0x%x \r\n 0X%x",udata->data[1],udata->data[2]);
         udata->flag = 0;
         udata->len = 0;
         return 1;
@@ -53,8 +52,8 @@ static void do_i2c_8RegisterAddr(uint8_t *data)
     case 0x01: // 写一个字节寄存器
         I2C_ByteWrite(slave_adress, RegisterAddr, *Value);
         break;
-    case 0x02: // 写多个字节寄存器
-        I2C_ByetsWrite(slave_adress, RegisterAddr, Value, NumByteToProsess);
+    case 0x02: // 写多个字节寄存器  待添加当实际接收到的写字节个数，少于NumByteToProsess时情景的判断
+        I2C_BufferWrite(slave_adress, RegisterAddr, Value, NumByteToProsess);
         break;
     case 0x03: // 读一个字节寄存器
         I2C_byteRead(slave_adress, RegisterAddr, Value);
@@ -67,6 +66,9 @@ static void do_i2c_8RegisterAddr(uint8_t *data)
             printf("RegisterAddr : 0x%02x Return Value : 0x%02x \r\n", RegisterAddr + i, Value[i]);
         }
         break;
+    case 0x05:
+        Find_i2c_device();
+        break;
     default:
         printf("Use 8bit RegisterAddr mode Error! \r\n");
         break;
@@ -75,6 +77,39 @@ static void do_i2c_8RegisterAddr(uint8_t *data)
 
 static void do_i2c_16RegisterAddr(uint8_t *data)
 {
+    uint8_t i2c_mode_sel = data[2];
+    uint8_t slave_adress = data[3];
+    uint8_t NumByteToProsess = data[4];
+    uint8_t RegisterAddr_H = data[5];
+    uint8_t RegisterAddr_L = data[6];
+    uint16_t RegisterAddr = (uint16_t)(RegisterAddr_H << 8 | RegisterAddr_L);
+    uint8_t *Value = &data[7];
+    switch (i2c_mode_sel)
+    {
+    case 0x01: // 写一个字节寄存器
+        I2C_Write_16addr(slave_adress, RegisterAddr, *Value);
+        break;
+    case 0x02: // 写多个字节寄存器  待添加当实际接收到的写字节个数，少于NumByteToProsess时情景的判断
+        I2C_BufferWrite_16addr(slave_adress, RegisterAddr, Value, NumByteToProsess);
+        break;
+    case 0x03: // 读一个字节寄存器
+        I2C_Read16addr(slave_adress, RegisterAddr, Value);
+        printf("RegisterAddr : 0x%04x Return Value = 0x%02x \r\n", RegisterAddr, *Value);
+        break;
+    case 0x04: // 读多个字节寄存器
+        I2C_BufferRead_16addr(slave_adress, RegisterAddr, Value, NumByteToProsess);
+        for (size_t i = 0; i < NumByteToProsess; i++)
+        {
+            printf("RegisterAddr : 0x%04x Return Value : 0x%02x \r\n", RegisterAddr + i, Value[i]);
+        }
+        break;
+    case 0x05:
+        Find_i2c_device();
+        break;
+    default:
+        printf("Use 16bit RegisterAddr mode Error! \r\n");
+        break;
+    }
 }
 
 static void do_spi(uint8_t *data)
