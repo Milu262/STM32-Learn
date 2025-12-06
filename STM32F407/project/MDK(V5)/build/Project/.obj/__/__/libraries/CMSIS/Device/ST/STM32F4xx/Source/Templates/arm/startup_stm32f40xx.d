04ad2bea85157d938ef68d83fdb8136c293f5885
Ankesiqi <1261807915@qq.com>
2025-12-06 23:42:31 +0800

    移除以前的缓存，不添加中间的编译文件（*.o)

--- STM32F407/project/MDK(V5)/build/Project/.obj/__/__/libraries/CMSIS/Device/ST/STM32F4xx/Source/Templates/arm/startup_stm32f40xx.d
+++
@@ -1,1 +0,0 @@
-.\build\Project\.obj\__\__\libraries\CMSIS\Device\ST\STM32F4xx\Source\Templates\arm\startup_stm32f40xx.o: .\..\..\libraries\CMSIS\Device\ST\STM32F4xx\Source\Templates\arm\startup_stm32f40xx.s
